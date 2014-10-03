2efefd3bcc05d1c2a2aa12d282188f642e7e9daf
SimulatedSimian <neomech@gmail.com>
2014-10-03 22:57:18 +0000

    added board style

--- script/app.js
+++ script/app.js
@@ -1,4 +1,23 @@
 
+var game = {
+	boardSize: 8
+};
+
+game.makeBoardHTML = function (parent) {
+    for(var tr = 0; tr < game.boardSize; tr++){
+        var row = $("<tr/>");
+        parent.append(row);
+        for(var td = 0; td < game.boardSize; td++){
+			if ((tr + td) & 1)
+            	row.append($("<td class='cell'><div class='piece white'></div></td>"));
+			else
+            	row.append($("<td class='cell'><div class='piece black'></div></td>"));
+				
+        }
+    }
+}
+
 $(document).ready( function () {
+	game.makeBoardHTML($(".board"))
 });
 
