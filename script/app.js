2049ade5bc305e716aa17949c67488bc173fa70f
SimulatedSimian <neomech@gmail.com>
2014-10-07 14:29:09 +0000

    initial working html game board visual

--- script/app.js
+++ script/app.js
@@ -1,23 +1,68 @@
-
 var game = {
-	boardSize: 8
+    boardSize: 8,
+
+    Piece: {
+        none: 0,
+        white: 1,
+        black: 2
+    }
 };
 
-game.makeBoardHTML = function (parent) {
-    for(var tr = 0; tr < game.boardSize; tr++){
+game.BoardVisual = function(parentElement) {
+    this.parentElement = parentElement;
+}
+
+game.BoardVisual.prototype.makeID = function(x, y) {
+    return "_" + x + "_" + y + "_";
+}
+
+game.BoardVisual.prototype.getPiece = function(x, y) {
+    return $(this.parentElement).find(".piece#" + this.makeID(x, y));
+}
+
+game.BoardVisual.prototype.makeBoardHTML = function() {
+    for(var tr = 0; tr < game.boardSize; tr++) {
         var row = $("<tr/>");
-        parent.append(row);
-        for(var td = 0; td < game.boardSize; td++){
-			if ((tr + td) & 1)
-            	row.append($("<td class='cell'><div class='piece white'></div></td>"));
-			else
-            	row.append($("<td class='cell'><div class='piece black'></div></td>"));
-				
+        this.parentElement.append(row);
+        for(var td = 0; td < game.boardSize; td++) {
+            row.append($("<td class='cell'><div class='piece' id='" + this.makeID(td, tr) + "'></div></td>"));
         }
     }
 }
 
-$(document).ready( function () {
-	game.makeBoardHTML($(".board"))
-});
+game.BoardVisual.prototype.resetBoard = function() {
+    $(this.parentElement).find(".piece").removeClass("black white");
+}
 
+game.BoardVisual.prototype.setPiece = function(x, y, piece) {
+    if(piece === game.Piece.none)
+        this.getPiece(x, y).removeClass("black white");
+
+    if(piece === game.Piece.white)
+        this.getPiece(x, y).removeClass("black").addClass("white");
+
+    if(piece === game.Piece.black)
+        this.getPiece(x, y).removeClass("white").addClass("black");
+}
+
+game.BoardVisual.prototype.resetPlayableCells = function() {
+    $(this.parentElement).find(".cell").removeClass("playable");
+}
+
+game.BoardVisual.prototype.setCellPlayable = function(x, y) {
+	this.getPiece(x, y).parent().addClass("playable");
+}
+
+game.BoardModel = function() {
+
+}
+
+game.BoardModel.prototype.setPiece = function(x, y, piece) {
+
+}
+
+
+$(document).ready(function() {
+    game.visual = new game.BoardVisual($(".board"));
+    game.visual.makeBoardHTML();
+});
