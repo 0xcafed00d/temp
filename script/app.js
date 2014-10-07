006b1e72f012f31c756667de0f079dcbf9606f2e
SimulatedSimian <neomech@gmail.com>
2014-10-07 23:08:16 +0000

    initial game model

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-var game = {
+var reversi = {
     boardSize: 8,
 
     Piece: {
@@ -8,61 +8,99 @@ var game = {
     }
 };
 
-game.BoardVisual = function(parentElement) {
+// ----------------------------------------------------------------------------------------
+// 	Board Visual
+// ----------------------------------------------------------------------------------------
+
+reversi.BoardVisual = function(parentElement) {
     this.parentElement = parentElement;
 }
 
-game.BoardVisual.prototype.makeID = function(x, y) {
+reversi.BoardVisual.prototype.makeID = function(x, y) {
     return "_" + x + "_" + y + "_";
 }
 
-game.BoardVisual.prototype.getPiece = function(x, y) {
+reversi.BoardVisual.prototype.getPiece = function(x, y) {
     return $(this.parentElement).find(".piece#" + this.makeID(x, y));
 }
 
-game.BoardVisual.prototype.makeBoardHTML = function() {
-    for(var tr = 0; tr < game.boardSize; tr++) {
+reversi.BoardVisual.prototype.makeBoardHTML = function() {
+    for(var tr = 0; tr < reversi.boardSize; tr++) {
         var row = $("<tr/>");
         this.parentElement.append(row);
-        for(var td = 0; td < game.boardSize; td++) {
+        for(var td = 0; td < reversi.boardSize; td++) {
             row.append($("<td class='cell'><div class='piece' id='" + this.makeID(td, tr) + "'></div></td>"));
         }
     }
 }
 
-game.BoardVisual.prototype.resetBoard = function() {
+reversi.BoardVisual.prototype.resetBoard = function() {
     $(this.parentElement).find(".piece").removeClass("black white");
+    this.setPiece(3, 3, reversi.Piece.white);
+    this.setPiece(4, 4, reversi.Piece.white);
+    this.setPiece(3, 4, reversi.Piece.black);
+    this.setPiece(4, 3, reversi.Piece.black);
 }
 
-game.BoardVisual.prototype.setPiece = function(x, y, piece) {
-    if(piece === game.Piece.none)
+reversi.BoardVisual.prototype.setPiece = function(x, y, piece) {
+    if(piece === reversi.Piece.none)
         this.getPiece(x, y).removeClass("black white");
 
-    if(piece === game.Piece.white)
+    if(piece === reversi.Piece.white)
         this.getPiece(x, y).removeClass("black").addClass("white");
 
-    if(piece === game.Piece.black)
+    if(piece === reversi.Piece.black)
         this.getPiece(x, y).removeClass("white").addClass("black");
 }
 
-game.BoardVisual.prototype.resetPlayableCells = function() {
+reversi.BoardVisual.prototype.resetPlayableCells = function() {
     $(this.parentElement).find(".cell").removeClass("playable");
 }
 
-game.BoardVisual.prototype.setCellPlayable = function(x, y) {
-	this.getPiece(x, y).parent().addClass("playable");
+reversi.BoardVisual.prototype.setCellPlayable = function(x, y) {
+    this.getPiece(x, y).parent().addClass("playable");
 }
 
-game.BoardModel = function() {
+// ----------------------------------------------------------------------------------------
+// 	Board Model
+// ----------------------------------------------------------------------------------------
 
+reversi.BoardModel = function() {
+    this.board = [];
+    for(var x = 0; x < reversi.boardSize; x++)
+        a.push(new Array(reversi.boardSize));
 }
 
-game.BoardModel.prototype.setPiece = function(x, y, piece) {
+reversi.BoardModel.prototype.resetBoard = function() {
+    for(var x = 0; x < reversi.boardSize; x++)
+        for(var y = 0; y < reversi.boardSize; y++)
+            this.board[x][y] = reversi.Piece.none;
 
+    this.setPiece(3, 3, reversi.Piece.white);
+    this.setPiece(4, 4, reversi.Piece.white);
+    this.setPiece(3, 4, reversi.Piece.black);
+    this.setPiece(4, 3, reversi.Piece.black);
 }
 
+reversi.BoardModel.prototype.setPiece = function(x, y, piece) {
+    this.board[x][y] = piece;
+}
+
+reversi.BoardModel.prototype.getPiece = function(x, y) {
+    return this.board[x][y];
+}
+
+// ----------------------------------------------------------------------------------------
+// 	Reversi Game Controller
+// ----------------------------------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------------------------------
+// 	Reversi Game Start
+// ----------------------------------------------------------------------------------------
 
 $(document).ready(function() {
-    game.visual = new game.BoardVisual($(".board"));
-    game.visual.makeBoardHTML();
+    reversi.visual = new reversi.BoardVisual($(".board"));
+    reversi.visual.makeBoardHTML();
 });
